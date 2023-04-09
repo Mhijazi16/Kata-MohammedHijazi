45442353a868dde5af18165ddfe50f38dc747870
Mhijazi16 <gultimate0@gmail.com>
2023-04-09 13:41:27 +0200

    Tax : added Tax field to customer class

--- Kata-MohammedHijazi/Customer.cs
+++ Kata-MohammedHijazi/Customer.cs
@@ -4,4 +4,11 @@ public class Customer
 {
     public string Name { get; set; } 
     public List<Product> Products { get; set; }
+    private decimal tax;
+
+    public decimal Tax
+    {
+        get => tax;
+        set => tax.ValidateTax(); 
+    }
 }

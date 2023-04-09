f3e502727fd3e751611026b152d91fcd8c680ef1
Mhijazi16 <gultimate0@gmail.com>
2023-04-09 13:28:16 +0200

    Tax : added Constructor & Dictionary as Price to track different state of price

--- Kata-MohammedHijazi/Product.cs
+++ Kata-MohammedHijazi/Product.cs
@@ -1,8 +1,26 @@
+using Kata_MohammedHijazi.ExtensionMethods;
+
 namespace Kata_MohammedHijazi;
 
 public class Product
 {
      public string Name { get; set; }
      public int UPC { get; set; }
-     public decimal Price { get; set; }
+     private Dictionary<PriceState, decimal> Prices { get; set; }
+     public decimal Price(PriceState state) => Prices[state];
+     public void Price(PriceState state, decimal price) => Prices[state] = price.ValidatePrice();  
+     
+     public Product()
+     {
+          Name = "Product-Unknown";
+          UPC = 0;
+          Prices = new Dictionary<PriceState, decimal>(); 
+          Price(PriceState.Normal, 1);
+     }
+     public Product(string name, int upc, Dictionary<PriceState, decimal> prices)
+     {
+          Name = name;
+          UPC = upc;
+          Prices = prices;
+     }
 }

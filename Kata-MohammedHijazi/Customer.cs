62499991ee71ea6635f19015ddcb70b14a85ff57
Mhijazi16 <gultimate0@gmail.com>
2023-04-09 13:50:30 +0200

    Tax : Created Tax Extension Methdos and Applied them into Customer List

--- Kata-MohammedHijazi/Customer.cs
+++ Kata-MohammedHijazi/Customer.cs
@@ -1,14 +1,30 @@
+using Kata_MohammedHijazi.ExtensionMethods;
+
 namespace Kata_MohammedHijazi;
 
 public class Customer
 {
     public string Name { get; set; } 
     public List<Product> Products { get; set; }
-    private decimal tax;
-
+    private decimal tax = 0.20m;
     public decimal Tax
     {
         get => tax;
         set => tax.ValidateTax(); 
+    }
+
+    public Customer()
+    {
+        Name = "Unkown-Customer";
+        Tax = 0.20m;
+        Products = new List<Product>();
+    }
+
+    public Customer(decimal tax, string name, List<Product> products)
+    {
+        this.tax = tax;
+        Name = name;
+        Products = products;
+        Products.ApplyTaxToList(tax);
     }
 }
--- Kata-MohammedHijazi/ExtensionMethods/PriceHandler.cs
+++ Kata-MohammedHijazi/ExtensionMethods/PriceHandler.cs
@@ -1,6 +1,11 @@
 namespace Kata_MohammedHijazi.ExtensionMethods;
 
-public class PriceHandler
+public static class PriceHandler
 {
-    
+   public static void ApplyTax(this Product product, decimal tax) => product.Price(PriceState.Taxed, tax);
+   public static void ApplyTaxToList(this List<Product> list, decimal tax)
+   {
+      foreach (var item in list)
+         item.ApplyTax(tax); 
+   }
 }

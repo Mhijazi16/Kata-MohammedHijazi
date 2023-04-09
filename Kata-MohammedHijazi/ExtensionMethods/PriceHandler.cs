4df57820805a75ac5e2e369de443f1497d9032b7
Mhijazi16 <gultimate0@gmail.com>
2023-04-09 14:23:58 +0200

    Tax : Optimizing the Extension Methods to Compute tax & net price

--- Kata-MohammedHijazi/ExtensionMethods/PriceHandler.cs
+++ Kata-MohammedHijazi/ExtensionMethods/PriceHandler.cs
@@ -2,10 +2,22 @@ namespace Kata_MohammedHijazi.ExtensionMethods;
 
 public static class PriceHandler
 {
-   public static void ApplyTax(this Product product, decimal tax) => product.Price(PriceState.Taxed, tax);
+   //For Computing The Taxed Price
+   public static decimal ComputeTaxAmount(this Product prod, decimal tax) => tax * prod.Price(PriceState.Normal);
+   public static void ApplyTax(this Product product, decimal tax)
+   {
+      decimal Taxed = product.ComputeTaxAmount(tax) + product.Price(PriceState.Normal);
+      product.Price(PriceState.Taxed, Taxed);
+   } 
    public static void ApplyTaxToList(this List<Product> list, decimal tax)
    {
       foreach (var item in list)
          item.ApplyTax(tax); 
+   }
+   //For Computing the Net Price
+   public static void ComputeNetPrice(this Product product)
+   {
+      decimal Taxed = product.Price(PriceState.Taxed);
+      product.Price(PriceState.Net, Taxed);
    }
 }

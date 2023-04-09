ca7cee7a54cd31c2f193f1e4959b522ff00435ae
Mhijazi16 <gultimate0@gmail.com>
2023-04-09 13:23:28 +0200

    Tax : add ValidatePrice() & SetPrecision() Methods to validate Prices

--- Kata-MohammedHijazi/ExtensionMethods/Validation.cs
+++ Kata-MohammedHijazi/ExtensionMethods/Validation.cs
@@ -6,5 +6,12 @@ public static class Validation
 {
    public static bool InRange(this decimal value, decimal lower, decimal upper) => value >=lower && value <= upper;
    public static bool IsPossitve(this decimal value) => value >= 0;
-   public static decimal SetPrecision(this decimal value) => Decimal.Round(value, 2); 
+   public static decimal SetPrecision(this decimal value) => Decimal.Round(value, 2);
+
+   public static decimal ValidatePrice(this decimal price)
+   {
+      if (price.InRange(0, 100) == false)
+         throw new ArgumentException("The Price Isn't In Correct Range (0-100)");
+      return price.SetPrecision(); 
+   }
 }

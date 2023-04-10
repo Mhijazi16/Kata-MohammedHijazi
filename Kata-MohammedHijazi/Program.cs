9d5ef36f5ab2a3b1cf0edba4dd07e218eff55561
Mhijazi16 <gultimate0@gmail.com>
2023-04-10 15:44:41 +0200

    Discount : Finishing up & Running The code

--- Kata-MohammedHijazi/Program.cs
+++ Kata-MohammedHijazi/Program.cs
@@ -6,8 +6,8 @@ public class Program
 {
     public static void Main()
     {
-        // Product p1 = new Product("AA",1234,20,15,20.25m);
-        Product p1 = new Product();
+        ProductInfo info = new ProductInfo("aa",123);
+        Product p1 = new Product(info, 15, 20.25m); 
         p1.PrintInfo();
     }
 }

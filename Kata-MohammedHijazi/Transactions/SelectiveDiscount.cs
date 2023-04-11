3795b179b358513cce7635942b73c3c7aef92de8
Mhijazi16 <gultimate0@gmail.com>
2023-04-11 15:19:21 +0200

    Selective : added Regions to Organize Code

--- Kata-MohammedHijazi/Transactions/SelectiveDiscount.cs
+++ Kata-MohammedHijazi/Transactions/SelectiveDiscount.cs
@@ -5,26 +5,44 @@ namespace Kata_MohammedHijazi.Transactions;
 
 public class SelectiveDiscount 
 {
-   public int SelectiveUPC { get; set;}
-   private decimal rate;
-   private decimal amount;
-   public bool isActiveated = false; 
-   public decimal Ratio{ get => rate; set => rate = value.ValidateRatio(); }
-   public decimal Amount { get => amount.SetPrecision() ; set => amount = rate * value; }
+    #region Fields
 
-   public SelectiveDiscount(int selectiveUpc, decimal ratio ,decimal price, int upc)
-   {
-      SelectiveUPC = selectiveUpc;
-      Ratio = ratio;
-      if (isActive(upc))
-          Amount = price; 
-   }   
+    public int SelectiveUPC { get; set;}
+    private decimal rate;
+    private decimal amount;
+    public bool isActiveated = false; 
 
-   public bool isActive(int upc) =>  isActiveated = upc == SelectiveUPC;
-   public decimal ApplySelective(decimal discountedPrice)
-   {
-       if(isActiveated)
-           return discountedPrice - Amount;
-       return 0; 
-   }
+    #endregion
+
+    #region Getters&Setters
+
+    public decimal Ratio{ get => rate; set => rate = value.ValidateRatio(); }
+    public decimal Amount { get => amount.SetPrecision() ; set => amount = rate * value; }
+
+    #endregion
+
+    #region Constructor
+
+    public SelectiveDiscount(int selectiveUpc, decimal ratio ,decimal price, int upc)
+    {
+       SelectiveUPC = selectiveUpc;
+       Ratio = ratio;
+       if (isActive(upc))
+           Amount = price; 
+    }   
+
+    #endregion
+
+    #region Methods
+
+    public bool isActive(int upc) =>  isActiveated = upc == SelectiveUPC;
+    public decimal ApplySelective(decimal discountedPrice)
+    {
+        if(isActiveated)
+            return discountedPrice - Amount;
+        return 0; 
+    }   
+
+    #endregion
+
 }

e978f28cad77d2316f297ff584e78156bf7bcac1
Mhijazi16 <gultimate0@gmail.com>
2023-04-11 15:14:23 +0200

    Selective : created Fields, Getters&Setters

--- Kata-MohammedHijazi/Transactions/SelectiveDiscount.cs
+++ Kata-MohammedHijazi/Transactions/SelectiveDiscount.cs
@@ -1,6 +1,16 @@
+using System.IO.Compression;
+using Kata_MohammedHijazi.ExtensionMethods;
+
 namespace Kata_MohammedHijazi.Transactions;
 
-public class SelectiveDiscount
+public class SelectiveDiscount 
 {
-    
+   public int SelectiveUPC { get; set;}
+   private decimal rate;
+   private decimal amount;
+   public bool isActiveated = false; 
+   public decimal Ratio{ get => rate; set => rate = value.ValidateRatio(); }
+   public decimal Amount { get => amount.SetPrecision() ; set => amount = rate * value; }
+
+
 }

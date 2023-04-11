91a41c2832d7acceea279cc15591979b4772533b
Mhijazi16 <gultimate0@gmail.com>
2023-04-11 23:01:35 +0200

    Expenses : Finishing up & Running The code

--- Kata-MohammedHijazi/Program.cs
+++ Kata-MohammedHijazi/Program.cs
@@ -6,13 +6,12 @@ public class Program
 {
     public static void Main()
     {
-        ProductInfo info = new ProductInfo("AA", 123);
+        ProductInfo info = new ProductInfo("AA", 123,new Expenses(0.01m,2.2m));
         Tax tax = new Tax(20.25m);
         SelectiveDiscount selective = new SelectiveDiscount(123, 7, 20.25m, info.UPC);
         Discount discount = new Discount(20.25m, 15, selective);
         Product p = new Product(info, tax, discount, 20.25m,true);
-
-        p.ReportSelectiveDiscount();
-        p.ReportPrecedence();
+        
+        p.ReportExpenses();
     }
 }

079f3b28711149e0c59f7aecc907e49af3edee9f
Mhijazi16 <gultimate0@gmail.com>
2023-04-11 23:00:06 +0200

    Expenses : made ProductInfo class use the Expense class

--- Kata-MohammedHijazi/ProductInfo.cs
+++ Kata-MohammedHijazi/ProductInfo.cs
@@ -1,13 +1,16 @@
+using Kata_MohammedHijazi.Transactions;
+
 namespace Kata_MohammedHijazi;
 
 public class ProductInfo
 {
      public string Name { get; set; }
      public int UPC { get; set; }
-
-     public ProductInfo(string name, int upc)
+     public Expenses Expenses { get; set; }
+     public ProductInfo(string name, int upc,Expenses expenses)
      {
          Name = name;
          UPC = upc;
+         Expenses = expenses;
      }
 }

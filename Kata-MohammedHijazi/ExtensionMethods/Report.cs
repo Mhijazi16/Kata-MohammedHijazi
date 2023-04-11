4d8191449b49933c7459d57cce4069a007bb4440
Mhijazi16 <gultimate0@gmail.com>
2023-04-11 15:29:09 +0200

    Selective : added ReportSelectiveDiscount() method to report the selective discount

--- Kata-MohammedHijazi/ExtensionMethods/Report.cs
+++ Kata-MohammedHijazi/ExtensionMethods/Report.cs
@@ -11,10 +11,10 @@ public static class Report
              decimal net = p.Price(PriceState.Net);
              
               Console.WriteLine("==============================================");
-              Console.WriteLine($"Name: {p.Info.Name} , UPC: {p.Info.UPC} , Price: {normal}");
-              Console.WriteLine($"Tax Amount: {p.Tax.Amount}, Discount Amount: {p.Discount.Amount},");
-              Console.WriteLine($"Price Before Tax = {normal}, After Tax : {taxed}");
-              Console.WriteLine($"After Discount : {discounted}, Net: {net}"); 
+              Console.WriteLine($"Name: {p.Info.Name} , UPC: {p.Info.UPC} , Price: ${normal}");
+              Console.WriteLine($"Tax Amount: {p.Tax.Amount}, Discount Amount: ${p.Discount.Amount},");
+              Console.WriteLine($"Price Before Tax = ${normal}, After Tax : ${taxed}");
+              Console.WriteLine($"After Discount : ${discounted}, Net: ${net}"); 
               Console.WriteLine("==============================================");
          }
 
@@ -23,9 +23,19 @@ public static class Report
              string replace = Prod.Discount.isApplied ? Prod.Discount.Amount + "" : "No Discount is Applied"; 
              
              Console.WriteLine("===============================================");
-             Console.WriteLine($"Tax Amount: {Prod.Tax.Amount}, Discount Amount: {replace}");
-             Console.WriteLine($"Original Price: {Prod.Price(PriceState.Normal)}");
-             Console.WriteLine($"Final Net Price: {Prod.Price(PriceState.Net)}");
+             Console.WriteLine($"Tax Amount: ${Prod.Tax.Amount}, Discount Amount: ${replace}");
+             Console.WriteLine($"Original Price: ${Prod.Price(PriceState.Normal)}");
+             Console.WriteLine($"Final Net Price: ${Prod.Price(PriceState.Net)}");
+             Console.WriteLine("===============================================");
+         }
+
+         public static void ReportSelectiveDiscount(this Product product)
+         {
+              Console.WriteLine("===============================================");   
+             Console.WriteLine($"Tax = {product.Tax.Ratio * 100}%, universal discount = {product.Discount.Ratio * 100}%, UPC-discount = {product.Discount.selectiveDiscount.Ratio * 100}% for UPC={product.Discount.selectiveDiscount.SelectiveUPC}");
+             Console.WriteLine($"Tax amount = ${product.Tax.Amount}, discount = ${product.Discount.Amount}, UPC discount = ${product.Discount.selectiveDiscount.Amount}");
+             Console.WriteLine($"Final Net Price: {product.Price(PriceState.Net)}");
+             Console.WriteLine($"Total Discount Amount: {product.Discount.Amount + product.Discount.selectiveDiscount.Amount}");
              Console.WriteLine("===============================================");
          }
             

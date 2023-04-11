499f5509aff1f2229ab315251b44662cf7d36368
Mhijazi16 <gultimate0@gmail.com>
2023-04-11 15:35:37 +0200

    Selective : Wrapping up & Running the code

--- Kata-MohammedHijazi/Program.cs
+++ Kata-MohammedHijazi/Program.cs
@@ -1,13 +1,17 @@
 ﻿
 using System.Net.NetworkInformation;
 using Kata_MohammedHijazi;
-
+using Kata_MohammedHijazi.Transactions;
 public class Program
 {
     public static void Main()
     {
-        ProductInfo info = new ProductInfo("aa",123);
-        Product p1 = new Product(info,15, 20.25m); 
-        p1.ReportDiscount();
+        ProductInfo info = new ProductInfo("AA", 123);
+        Tax tax = new Tax(20.25m);
+        SelectiveDiscount selective = new SelectiveDiscount(123, 7, 20.25m, info.UPC);
+        Discount discount = new Discount(20.25m, 15, selective);
+        Product p = new Product(info, tax, discount, 20.25m);
+
+        p.ReportSelectiveDiscount();
     }
 }

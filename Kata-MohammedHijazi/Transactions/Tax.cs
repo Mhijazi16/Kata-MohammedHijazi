d28c8c89406b16009db3c6a54fd3bd85318089f8
Mhijazi16 <gultimate0@gmail.com>
2023-04-13 14:43:32 +0200

    Refactor : added ValueManipulator abstract class for the tax & discount to inhirate from

--- Kata-MohammedHijazi/Transactions/Tax.cs
+++ Kata-MohammedHijazi/Transactions/Tax.cs
@@ -1,51 +1,19 @@
-using Kata_MohammedHijazi.ExtensionMethods;
 
 namespace Kata_MohammedHijazi.Transactions;
 
-public class Tax
+public class Tax : ValueManipulator
 {
-    #region Fields
-
-    private decimal ratio;
-    private decimal amount;
-
-    #endregion
-
-    #region Getters&Setters 
-
-    public decimal Ratio
-    {
-        get => ratio;
-        set => ratio = value.ValidateRatio();
-    }
-
-    public decimal Amount
-    {
-        get => amount;
-        set => amount = (value * Ratio).SetPrecision();
-    }
-
-    #endregion
-
-    #region Constructors
-
     public Tax(decimal price)
     {
         Ratio = 0.2m;
-        Amount = price; 
+        Amount = price * Ratio; 
     }
-
     public Tax(decimal price, decimal ratio)
     {
         Ratio = ratio;
-        Amount = price; 
+        Amount = price * Ratio; 
     }
-
-    #endregion
-
-    #region Methods
 
     public decimal ApplyTax(decimal price) => price + Amount;
 
-    #endregion
 }
---
+++ Kata-MohammedHijazi/Transactions/ValueManipulator.cs
@@ -0,0 +1,21 @@
+using Kata_MohammedHijazi.ExtensionMethods;
+
+namespace Kata_MohammedHijazi.Transactions;
+
+public abstract class ValueManipulator
+{
+    private decimal _ratio;
+    private decimal _amount; 
+    
+    public decimal Ratio
+    {
+        get => _ratio;
+        set => _ratio = value.ValidateRatio();
+    }
+
+    public decimal Amount
+    {
+        get => _amount;
+        set => _amount = value.SetPrecision();
+    }
+}

a61ec0dfd10156fdde7ac469302905dd26ce76cd
Mhijazi16 <gultimate0@gmail.com>
2023-04-14 15:43:12 +0200

    Combaining : Finishing up & Running the Code

--- Kata-MohammedHijazi/ExtensionMethods/PriceHandler.cs
+++ Kata-MohammedHijazi/ExtensionMethods/PriceHandler.cs
@@ -1,48 +1,57 @@
-using System.Runtime.CompilerServices;
 using Kata_MohammedHijazi.Transactions;
+using Kata_MohammedHijazi.Transactions.Discount;
 
 namespace Kata_MohammedHijazi.ExtensionMethods;
 
 public static class PriceHandler
 {
-
-   #region SettingUpPrices 
-   public static void InitializePrices(this Product prod, decimal price)
-    {
-       prod.Price(PriceState.Normal, price);
-       prod.Price(PriceState.Taxed, 0);
-       prod.Price(PriceState.Discounted, 0);
-       prod.Price(PriceState.Net, 0); 
-    }
- 
-    public static void SetupPricesWithPrecedence(this Product prod,decimal price)
-    {
-        prod.Discount.selectiveDiscount.Amount = price;
-        prod.Tax.Amount = prod.Discount.selectiveDiscount.ApplySelective(price);
-        prod.Discount.Amount = prod.Discount.selectiveDiscount.ApplySelective(price); 
-        decimal Expenses = prod.Info.Expenses.ComputeExpenses(price); 
-        
-        prod.Price(PriceState.Normal, price);
-        prod.Price(PriceState.Taxed, prod.Tax.ApplyTax(price));
-        prod.Price(PriceState.Discounted, prod.Discount.ApplyDiscount(price));
-        prod.Price(PriceState.Selective, prod.Discount.selectiveDiscount.ApplySelective(price));    
-        prod.Price(PriceState.Net,Expenses + price - prod.Discount.selectiveDiscount.Amount + prod.Tax.Amount - prod.Discount.Amount);
-    }
-     public static void SetupPrices(this Product prod,decimal price)
-     {
-         decimal Taxed = prod.Tax.ApplyTax(price);
-         decimal Discounted = prod.Discount.ApplyDiscount(Taxed);
-         decimal Selective = prod.Discount.selectiveDiscount.ApplySelective(Discounted);
-         decimal Net = Selective.ReturnNotZero(Discounted, Taxed);
-         decimal Expenses = prod.Info.Expenses.ComputeExpenses(price); 
-         
-         prod.Price(PriceState.Normal, price);
-         prod.Price(PriceState.Taxed, Taxed);
-         prod.Price(PriceState.Discounted, Discounted);
-         prod.Price(PriceState.Selective, Selective);    
-         prod.Price(PriceState.Net, Net + Expenses);
-     }   
     
-   #endregion
+    #region AssignPrices 
+
+    public static void AssignPrices(this Product product, dynamic values)
+    {
+        product.Price(PriceState.Normal, values.normal);
+        product.Price(PriceState.Net, values.net); 
+    }
+
+    #endregion
+
+    #region SettingUpPrices
+    
+    public static void SetupDefault(this Product product, decimal price)
+    {
+        decimal net = product.ComputeNetPrice(price); 
+        product.AssignPrices(new {normal = price, net = net});
+    }
+    public static void SetupAdditive(this Product product, decimal price)
+    {
+        product.Discount = new AdditiveDiscount(price, product.Discount.Ratio, product.Info.Upc);
+        decimal net = product.ComputeNetPrice(price); 
+        product.AssignPrices(new {normal = price, net = net});
+        Console.WriteLine(product.Discount.Amount);
+    }
+    public static void SetupMultiplicative(this Product product, decimal price)
+    {
+        product.Discount = new Multiplicative(price, product.Discount.Ratio, product.Info.Upc);
+        decimal net = product.ComputeNetPrice(price);  
+        product.AssignPrices(new {normal = price, net = net}); 
+    }
+    public static void SetupPrecedence(this Product product, decimal price)
+    {
+        if (product.Info.Upc.ValidateUpc() == true)
+        {
+            decimal selectiveAmount = price * Discount.SelectiveUpc.Value;
+            product.Tax = new Tax(price - selectiveAmount, product.Tax.Ratio);
+            product.Discount = new Discount(price - selectiveAmount, product.Discount.Ratio);
+            product.Discount.Amount += selectiveAmount; 
+        }
+
+        decimal net = product.ComputeNetPrice(price);
+        product.AssignPrices(new {normal = price, net = net});
+    }
+
+    #endregion
+    public static decimal ComputeNetPrice(this Product product, decimal price) =>
+        price + product.Tax.Amount - product.Discount.Amount + product.Info.Expenses.Amount; 
 
 }

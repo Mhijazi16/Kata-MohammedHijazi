0b87b764cea027655d9e36691abc0e4ef3f01511
Mhijazi16 <gultimate0@gmail.com>
2023-04-15 13:15:26 +0200

    Cap : added CheckForCap() which verifies if cap is a percent or amount and based on that it assign values for either ratio or amount

--- Kata-MohammedHijazi/Transactions/Discount/Cap.cs
+++ Kata-MohammedHijazi/Transactions/Discount/Cap.cs
@@ -7,6 +7,13 @@ public class Cap
     public static decimal Value { get; set; }
     public static bool isRatio { get; set; }  
 
+    public static decimal CheckForCap(decimal value, bool ratio)
+    {
+        isRatio = Value.IsPercentage();
+        if (isRatio == ratio)
+            return ComputeForCap(value);
+        return value;
+    }
     private static decimal ComputeForCap(decimal value)
         {
             if (value >= Value)

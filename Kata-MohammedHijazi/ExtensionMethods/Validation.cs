bf58f1cfe2792b5fb8cdca04d0b0a6c018277b62
Mhijazi16 <gultimate0@gmail.com>
2023-04-15 13:09:10 +0200

    Cap : created IsPercentage() to check if value is percent & removed IsNotZero() method

--- Kata-MohammedHijazi/ExtensionMethods/Validation.cs
+++ Kata-MohammedHijazi/ExtensionMethods/Validation.cs
@@ -5,22 +5,15 @@ namespace Kata_MohammedHijazi.ExtensionMethods;
 
 public static class Validation
 {
+   #region GeneralValidationMethods
    public static bool InRange(this decimal value, decimal lower, decimal upper) => value >=lower && value <= upper;
    public static bool IsPossitve(this decimal value) => value >= 0;
    public static bool IsLargerThan(this decimal value, decimal min) => value > min; 
    public static decimal SetPrecision(this decimal value) => Decimal.Round(value, 2);
-   public static bool isNotZero(this decimal value) => value != 0;
+   public static bool IsPercentage(this decimal value) => value > 0 && value < 1;
+
+   #endregion
    public static bool ValidateUpc(this int value) => Discount.SelectiveUpc.Key == value; 
-
-   public static decimal ReturnNotZero(this decimal val1, decimal val2, decimal val3)
-   {
-      if (val1.isNotZero())
-         return val1;
-      if (val2.isNotZero())
-         return val2;
-      return val3;
-   }
-
    public static decimal ValidatePrice(this decimal price)
    {
       if (price.IsPossitve() == false)

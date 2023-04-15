244fa6e4b496ea092e218821770fa25bfefaa1b8
Mhijazi16 <gultimate0@gmail.com>
2023-04-15 13:11:22 +0200

    Cap : created Cap class with static fields Value & isRatio

---
+++ Kata-MohammedHijazi/Transactions/Discount/Cap.cs
@@ -0,0 +1,12 @@
+using Kata_MohammedHijazi.ExtensionMethods;
+
+namespace Kata_MohammedHijazi.Transactions.Discount;
+
+public class Cap
+{
+    public static decimal Value { get; set; }
+    public static bool isRatio { get; set; }  
+
+
+
+}

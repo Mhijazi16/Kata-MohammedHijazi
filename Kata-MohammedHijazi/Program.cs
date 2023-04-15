aaae8782595fee81a65d0f8ffa3b960b6b67eed4
Mhijazi16 <gultimate0@gmail.com>
2023-04-15 13:18:39 +0200

    Cap : Running the Code & Finishing Up

--- Kata-MohammedHijazi/Program.cs
+++ Kata-MohammedHijazi/Program.cs
@@ -6,15 +6,17 @@ public class Program
 {
     public static void Main()
     {
-        Discount.SelectiveUpc = new KeyValuePair<int, decimal>(123, 0.07m); 
+        Discount.SelectiveUpc = new KeyValuePair<int, decimal>(123, 0.07m);
+        Cap.Value = 0.3m; 
         
         Product p1 = new Product(
             new ProductInfo("AA", 123, new Expenses(0,0,20.25m)),
-            new Tax(20.25m, 20),
+            new Tax(20.25m, 21),
             new Discount(20.25m, 15),
             20.25m,
-            ComputeState.Precedence);
+            ComputeState.Additive);
 
+            
             p1.GeneralReport(); 
     }
 }
